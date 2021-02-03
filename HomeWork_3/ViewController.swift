203dc3a8a89257ef7f42ffd686165d8a1356395d
Shangin Vyacheslav <slava-poison@yandex.ru>
2021-02-03 20:43:52 +0400

    Initial Commit

---
+++ HomeWork_3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork_3
+//
+//  Created by Вячеслав Шангин on 03.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
